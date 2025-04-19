917bbf5354e94076faf75339dea351717ca3a181
6oncvlo <goncaloarv@gmail.com>
2025-04-19 15:16:34 +0100

    key

--- src/data/load.py
+++ src/data/load.py
@@ -5,7 +5,7 @@ column_names = {
     'data': ["user_id", "item_id", "rating", "timestamp"]
     , 'user': ["user_id", "age", "gender", "occupation", "zip_code"]
     , 'item': [
-        "movie_id", "movie_title", "release_date", "video_release_date", "imdb_url"
+        "item_id", "movie_title", "release_date", "video_release_date", "imdb_url"
         , "action", "adventure", "animation", "children", "comedy"
         , "crime", "documentary", "drama", "fantasy", "film_noir"
         , "horror", "musical", "mystery", "romance", "sci_fi"
