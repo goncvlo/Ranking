d4b453bb96d37382cb1cc9afbffd70a42157363f
6oncvlo <goncaloarv@gmail.com>
2025-04-20 18:52:52 +0100

    features

---
+++ src/features/utils.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def prepare_features(
+        ratings: pd.DataFrame, features: dict[str, pd.DataFrame]
+        ) -> dict[str, pd.DataFrame | list]:
+    """Create X, y, and group sets."""
+
+    data = {}
+
+    # number of ratings per user
+    data['group'] = ratings.groupby('user_id').size().to_list()
+    # user and item features, and ratings
+    data['X'] = (
+        ratings.copy()
+        .merge(features['user'], on='user_id', how='left')
+        .merge(features['item'], on='item_id', how='left')
+        .drop(columns=['user_id', 'item_id', 'rating', 'timestamp'])
+        )
+    data['y'] = ratings['rating']
+
+    return data
