3fbc655028ce502a96a4d0962be3c9021c1edf8d
6oncvlo <goncaloarv@gmail.com>
2025-04-25 01:49:24 +0100

    base train

--- main/train.py
+++ main/train.py
@@ -7,7 +7,7 @@ from src.data.load import load_data
 from src.data.prepare import prepare_data
 from src.models.cv_iterator import leave_last_k
 from src.data.features import feature_engineering
-from src.models.candidate import candidate_generation
+from src.models.retrieval import candidate_generation
 from src.data.utils import build_rank_input
 
 # read config
@@ -22,10 +22,12 @@ dataframes = prepare_data(dataframes=dataframes)
 # create user-item features
 user_item_features = feature_engineering(dataframes=dataframes)
 # perform positive and negative sampling
-df_train_neg = candidate_generation(dataframes['data'], n=30, positive_sampling=False)
-df_train_pos = candidate_generation(dataframes['data'], n=10, positive_sampling=True)
-df_train = pd.concat([dataframes['data'].iloc[:,:3], df_train_neg, df_train_pos], ignore_index=True)
-del df_train_neg, df_train_pos
+candidates = candidate_generation(dataframes['data'], config['model']['retrieval'])
+df_train = pd.concat(
+    [dataframes['data'].iloc[:,:3], candidates['positive'], candidates['negative']]
+    , ignore_index=True
+    )
+del candidates
 
 df_train = build_rank_input(ratings=df_train, features=user_item_features)
 
