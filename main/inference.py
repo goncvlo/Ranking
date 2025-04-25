72671fccae13e5a965fe34d96ad4effe8ed25742
6oncvlo <goncaloarv@gmail.com>
2025-04-25 19:32:49 +0100

    inference pipeline

---
+++ main/inference.py
@@ -0,0 +1,56 @@
+import yaml
+import pandas as pd
+import joblib
+
+from src.data.load import load_data
+from src.data.prepare import prepare_data
+from src.features.features import feature_engineering
+
+# read config
+with open('..\main\config.yml', 'r') as file:
+    config=yaml.load(file, Loader= yaml.SafeLoader)
+del file
+
+def inference(config: dict, user_ids: list) -> pd.DataFrame:
+    """Inference pipeline."""
+
+    # load and prepare data
+    dataframes = load_data(config=config['data_loader'])
+    dataframes = prepare_data(dataframes=dataframes)
+
+    # generate candidates from all the missing items
+    candidates = pd.MultiIndex.from_product(
+        [dataframes['data']['user_id'].unique(), dataframes['data']['item_id'].unique()]
+        , names=['user_id', 'item_id']
+        ).to_frame(index=False)
+
+    candidates = candidates.merge(
+        dataframes['data'][['user_id', 'item_id']].copy()
+        , on=['user_id', 'item_id'], how='left', indicator=True
+        )
+
+    candidates = candidates[
+        (candidates['_merge'] == 'left_only')
+        & (candidates['user_id'].isin(user_ids))
+        ][['user_id', 'item_id']]
+
+    # create features
+    user_item_features = feature_engineering(dataframes=dataframes)
+    candidates = (
+        candidates
+        .merge(user_item_features['user'], how='left', on='user_id')
+        .merge(user_item_features['item'], how='left', on='item_id')
+        )
+
+    # load model and get top-n recommendations
+    clf = joblib.load(config['model']['path'])
+    candidates['pred'] = clf.predict(candidates.drop(columns=['user_id', 'item_id']))
+
+    candidates = (
+        candidates
+        .sort_values(['user_id', 'pred'], ascending=[True, False])
+        .groupby('user_id').head(3)
+        [['user_id', 'item_id', 'pred']]
+        )
+    
+    return candidates
