5a2fb7a8777b21bcf73bb8fdea61eb5b17a64c14
6oncvlo <goncaloarv@gmail.com>
2025-04-29 23:29:33 +0100

    unit tests

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -3,7 +3,7 @@ from main.inference import inference
 
 @pytest.mark.performance
 def test_inference_speed_hard_limit(benchmark):
-    result = benchmark(inference, user_ids=[1, 2, 3])
+    result = benchmark(inference, user_id=28)
     
     assert result.shape[0] <= 9
     assert list(result.columns) == ['user_id', 'item_id', 'score']
