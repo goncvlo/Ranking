491d454b4b906882f7a41e942d9894b7447785b8
6oncvlo <goncaloarv@gmail.com>
2025-05-12 21:24:01 +0100

    ranker class

---
+++ src/models/ranker.py
@@ -0,0 +1,32 @@
+from xgboost import XGBRanker
+from lightgbm import LGBMRanker
+#from catboost import CatBoostRanker, Pool
+
+# supported algorithms
+algorithms = {
+    "XGBRanker": XGBRanker,
+    "LGBMRanker": LGBMRanker
+}
+
+class Ranker:
+    """Ranking model class."""
+
+    def __init__(self, algorithm: str, params: dict = None):
+        self.algorithm = algorithm
+        self.params = params or {}
+
+        if self.algorithm in algorithms:
+            self.model = algorithms[self.algorithm](**self.params)
+        else:
+            raise NotImplementedError(
+                f"{algorithm} isn't supported. Select from {list(algorithms.keys())}."
+            )
+    
+    def fit(self, X, y, group=None):
+        """Fit ranking model."""
+        self.model.fit(X=X, y=y, group=group)
+
+    def predict(self, X):
+        """Predict with ranking model."""
+        return self.model.predict(X=X)
+    
