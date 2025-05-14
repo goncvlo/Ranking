e7ff0933874cd17de4a78357920e3da6b11f064b
6oncvlo <goncaloarv@gmail.com>
2025-05-14 20:05:20 +0100

    input data

--- src/data/load.py
+++ src/data/load.py
@@ -1,31 +1,31 @@
 import pandas as pd
 
-# column names of three datasets
-column_names = {
-    'data': ["user_id", "item_id", "rating", "timestamp"]
-    , 'user': ["user_id", "age", "gender", "occupation", "zip_code"]
-    , 'item': [
-        "item_id", "movie_title", "release_date", "video_release_date", "imdb_url"
-        , "action", "adventure", "animation", "children", "comedy"
-        , "crime", "documentary", "drama", "fantasy", "film_noir"
-        , "horror", "musical", "mystery", "romance", "sci_fi"
-        , "thriller", "war", "western", "unknown"
-        ]
-        }
+COLUMN_NAMES = {
+    'data': ["user_id", "item_id", "rating", "timestamp"],
+    'user': ["user_id", "age", "gender", "occupation", "zip_code"],
+    'item': [
+        "item_id", "movie_title", "release_date", "video_release_date", "imdb_url",
+        "unknown", "action", "adventure", "animation", "children", "comedy",
+        "crime", "documentary", "drama", "fantasy", "film_noir", "horror",
+        "musical", "mystery", "romance", "sci_fi", "thriller", "war", "western"
+    ]
+}
 
 def load_data(config: dict) -> dict[str, pd.DataFrame]:
-    """Load ratings, items and users datasets."""
+    """Load ratings, items, and users datasets from local directory."""
 
-    dataframes ={}
+    dataframes = {}
 
-    for df_name in config['sub_paths']:
-        
-        sep = ['\t' if df_name=='data' else '|'][0]
+    # load each dataset as defined in the sub_paths
+    for key in config['sub_paths']:
+        sep = '\t' if key == 'data' else '|'
 
-        dataframes[df_name] = pd.read_csv(
-            config['path'] + df_name
-            , sep=sep, header=None, encoding='latin-1'
-            , names = column_names[df_name]
+        dataframes[key] = pd.read_csv(
+            f"{config['path']}/u.{key}",
+            sep=sep,
+            header=None,
+            names=COLUMN_NAMES[key],
+            encoding='latin-1'
         )
 
     return dataframes
