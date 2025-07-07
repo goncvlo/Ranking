3c10b699f56164ace4ecca4a2d637295bad5b8db
6oncvlo <goncaloarv@gmail.com>
2025-07-08 00:52:02 +0100

    logging class

--- src/models/tracker.py
+++ src/models/tracker.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import mlflow
 import subprocess
 import webbrowser
-import optuna
+from optuna.study import Study
 from surprise import AlgoBase
 from xgboost import XGBModel
 from lightgbm import LGBMModel
@@ -17,49 +17,110 @@ from src.models.ranker import Ranker
 from src.models.retrieval import Retrieval
 
 
-def log_run(
-        experiment_name: str,
-        study: optuna.study.Study,
-        tuner: BayesianSearch
-        ):
-    
-    # objects to be logged
-    params = study.best_trial.params | tuner.param_grid["fixed"]
-    metrics = {
-        "duration_secs": study.best_trial.duration.total_seconds(),
-        "trial_index": study.best_trial.number,
-        tuner.scoring_metric: study.best_trial.value
-        }
-    
-    with get_or_create_run(run_name=tuner.algorithm, experiment_name=experiment_name) as parent:
-        today_date = datetime.now().strftime("%d%b%Y").upper()
-        with mlflow.start_run(run_name=today_date, nested=True):
+class Logging:
 
-            log_artifact(params, "params")
-            log_artifact(metrics, "metrics")
-            log_artifact(tuner.param_grid, "param_grid")
-            log_artifact(study.trials_dataframe(), "bayes_search", "stats")
-            log_artifact(tuner.artifacts["test_evaluation"], "evaluation_metrics_test", "stats")
+    def __init__(self, experiment_name: str, run_name: str, input_sample: pd.DataFrame = None):
+        self.experiment_name = experiment_name
+        self.run_name = run_name
+        self.input_sample = input_sample
 
-            # convert trials results dictionary to dataframe
-            results = []
-            for split_id, split_data in tuner.artifacts["evaluation_metrics"].items():
-                for dataset, metric in split_data.items():
-                    row = {"trial_index": split_id, "metric": dataset}
-                    row.update(metric)
-                    results.append(row)
-            results = pd.DataFrame(results)
-            log_artifact(results, "evaluation_metrics_valid", "stats")
+    def log_run(
+            self,
+            study: Study = None, tuner: BayesianSearch = None,
+            clf: Union[Retrieval, Ranker] = None
+            ):
 
-            # log best validation model and test model
-            for i_model, name in zip([-1, metrics["trial_index"]], ["valid", "test"]):
-                log_model(
-                    artifact=tuner.artifacts["models"][i_model],
-                    artifact_name=f"model_{name}",
-                    input_sample=tuner.input_sample
+        with get_run(self.experiment_name, self.run_name) as parent:
+            now = datetime.now().strftime("%d%b%Y_%H%M%S").upper()
+            with mlflow.start_run(run_name=now, nested=True):
+
+                if study and tuner:
+                    self._log_tuner(study=study, tuner=tuner)
+                elif clf:
+                    self._log_training(clf=clf)
+
+    def _log_tuner(self, study: Study, tuner: BayesianSearch):
+
+        # objects to be logged
+        metrics = {
+            "duration_secs": study.best_trial.duration.total_seconds(),
+            "trial_index": study.best_trial.number,
+            tuner.metric: study.best_trial.value
+            }
+        
+        # logging
+        mlflow.log_params(params=tuner.artifacts["models"][-1].params)
+        mlflow.log_metrics(metrics=metrics)
+        mlflow.log_dict(dictionary=tuner.param_grid, artifact_file="param_grid.yaml")
+        self._log_df(study.trials_dataframe(), "bayes_search", "stats")
+        self._log_df(tuner.artifacts["metrics_test"], "metrics_test", "stats")
+
+        # convert trials results dictionary to dataframe
+        results = []
+        for split_id, split_data in tuner.artifacts["metrics_valid"].items():
+            for dataset, metric in split_data.items():
+                row = {"trial_index": split_id, "metric": dataset}
+                row.update(metric)
+                results.append(row)
+        results = pd.DataFrame(results)
+        self._log_df(results, "metrics_valid", "stats")
+
+        # log best validation model and test model
+        for i_model, name in zip([-1, metrics["trial_index"]], ["valid", "test"]):
+            self._log_model(
+                clf=tuner.artifacts["models"][i_model],
+                name=f"{tuner.algorithm}_{name}",
+                input_sample=self.input_sample
+                )
+            
+    def _log_training(self, clf: Union[Retrieval, Ranker]):
+
+        mlflow.log_params(params=clf.params)
+        self._log_model(clf=clf, name=clf.algorithm, input_sample=self.input_sample)
+
+    def _log_df(self, df: pd.DataFrame, name: str, path: str):
+
+        df.to_csv(f"{name}.csv")
+        mlflow.log_artifact(f"{name}.csv", artifact_path=path)
+        os.remove(f"{name}.csv")
+
+    def _log_model(
+            self,
+            clf: Union[Retrieval, Ranker], name: str,
+            input_sample: Union[pd.DataFrame, None] = None
+            ):
+        
+        model = clf.model
+
+        if isinstance(model, AlgoBase):
+            with open(f"{name}.pkl", "wb") as f:
+                pickle.dump(model, f)
+            mlflow.log_artifact(f"{name}.pkl", artifact_path=name)
+            os.remove(f"{name}.pkl")
+        
+        else:
+            output_sample = clf.predict(input_sample)
+            input_sample = input_sample.astype({col: 'float' for col in input_sample.select_dtypes(include='int').columns})
+            signature = mlflow.models.signature.infer_signature(input_sample, output_sample)
+            
+            if isinstance(model, XGBModel):
+                mlflow.xgboost.log_model(
+                    model,
+                    name=name,
+                    input_example=input_sample,
+                    signature=signature,
+                    model_format="json"
+                )
+
+            elif isinstance(model, LGBMModel):
+                mlflow.lightgbm.log_model(
+                    model,
+                    name=name,
+                    input_example=input_sample,
+                    signature=signature
                     )
-
-
+    
+                
 def launch_mlflow():
     """Launch MLflow UI on localhost and open it in a browser."""
     subprocess.Popen(["mlflow", "ui"])
@@ -69,24 +130,7 @@ def launch_mlflow():
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
 
-def log_artifact(artifact: Union[pd.DataFrame, dict, float, int], artifact_name: str, artifact_path: str = "default"):
-    """Save object locally, log it as an artifact, and delete it."""
-
-    if isinstance(artifact, pd.DataFrame):
-        artifact.to_csv(f"{artifact_name}.csv")
-        mlflow.log_artifact(f"{artifact_name}.csv", artifact_path=artifact_path)
-        os.remove(f"{artifact_name}.csv")
-    
-    if isinstance(artifact, dict):
-        if artifact_name=="params":
-            mlflow.log_params(params=artifact)
-        elif artifact_name=="metrics":
-            mlflow.log_metrics(metrics=artifact)
-        else:
-            mlflow.log_dict(dictionary=artifact, artifact_file=f"{artifact_name}.yaml")
-
-
-def get_or_create_run(run_name: str, experiment_name: str):
+def get_run(experiment_name: str, run_name: str):
     client = mlflow.tracking.MlflowClient()
 
     # get or create experiment
@@ -113,43 +157,6 @@ def get_or_create_run(run_name: str, experiment_name: str):
         # create new run in the selected experiment
         return mlflow.start_run(run_name=run_name)
     
-
-def log_model(
-        artifact: Union[Retrieval, Ranker],
-        artifact_name: str,
-        input_sample: pd.DataFrame
-        ):
-
-    model = artifact.model
-
-    if isinstance(model, AlgoBase):
-        with open(f"{artifact_name}.pkl", "wb") as f:
-            pickle.dump(model, f)
-        mlflow.log_artifact(f"{artifact_name}.pkl", artifact_path=artifact_name)
-        os.remove(f"{artifact_name}.pkl")
-
-    else:
-        output_example = artifact.predict(input_sample)
-        input_example = input_sample.astype({col: 'float' for col in input_sample.select_dtypes(include='int').columns})
-        signature = mlflow.models.signature.infer_signature(input_example, output_example)
-            
-        if isinstance(model, XGBModel):
-            mlflow.xgboost.log_model(
-                model,
-                name=artifact_name,
-                input_example=input_example,
-                signature=signature,
-                model_format="json"
-            )
-
-        if isinstance(model, LGBMModel):
-            mlflow.lightgbm.log_model(
-                model,
-                name=artifact_name,
-                input_example=input_example,
-                signature=signature
-                )
-
 
 def load_model(path: str, algorithm: str) -> Union[AlgoBase, XGBModel, LGBMModel]:
 
