9fcce9917cbedde2585dd78319192e5817ba751d
6oncvlo <goncaloarv@gmail.com>
2025-07-25 19:31:31 +0100

    user id encoded

--- src/data/prepare.py
+++ src/data/prepare.py
@@ -4,6 +4,11 @@ import pandas as pd
 def prepare_data(dataframes: dict[str, pd.DataFrame], config: dict) -> dict[str, pd.DataFrame]:
     """Transform dataframes."""
 
+    # user encoder mapping
+    user2id = {uid: i for i, uid in enumerate(dataframes["data"]["user_id"].unique())}
+    for df in ["data", "user"]:
+        dataframes[df]["user_id_encoded"] = dataframes[df]["user_id"].map(user2id)
+
     # user set
     dataframes["user"]["occupation"] = np.where(
         dataframes["user"]["occupation"]=="none"
